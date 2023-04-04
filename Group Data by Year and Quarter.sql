d47f911363055438a040356f754821dd3b4a9371
Ean Jimenez <99859369+Prometean@users.noreply.github.com>
2023-04-04 15:54:22 -0400

    Group Data by Year and Quarter
    Economic information typically requires categorization into designated time intervals or temporal bins. When producing financial statements, it's essential to categorize data into years and quarters.
    
    I intend to categorize the data based on years and quarters, highlighting the transaction count.
    
    The implemented code employs the EXTRACT() function to obtain the year and quarter data. This function adheres to the standard SQL syntax and can be applied in MySQL and PostgreSQL. However, in case you are using SQL Server, you should use the DATEPART() function as an alternative to EXTRACT().

--- Group Data by Year and Quarter.sql
+++ Group Data by Year and Quarter.sql
@@ -2,5 +2,6 @@ SELECT  DATEPART(YEAR, date) AS year,
         DATEPART(QUARTER, date) AS quarter,
         COUNT(amount) AS number_of_transactions
 FROM card_transaction
+
 GROUP BY EXTRACT(YEAR FROM date), EXTRACT(QUARTER FROM date)
 ORDER BY EXTRACT(YEAR FROM date) ASC, EXTRACT(QUARTER FROM date);
